f2a80797d5ad00e75d1f9cb4c62bbd9cd5a025ee
EricRootLee <erickiokom@gmail.com>
2021-07-25 12:17:31 +0300

    UPloaded imges succesfully

--- lib/src/data/database/database_client.dart
+++ lib/src/data/database/database_client.dart
@@ -1,19 +1,15 @@
-import 'dart:developer';
 import 'dart:typed_data';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:crime_map/src/models/crime_location_model.dart';
-import 'package:crime_map/src/provider/config/base_provider.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter/foundation.dart';
 import 'package:multi_image_picker/multi_image_picker.dart';
 
 import '../../helpers/common/app_constants.dart';
+import '../../models/crime_location_model.dart';
 
 class FirebaseClient {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  List<String> uploadUrlsLIst = [];
   Future saveUser(String userId, Map<String, dynamic> user) => _firestore
       .collection(AppConstants.usersCollections)
       .doc(userId)
@@ -28,37 +24,6 @@ class FirebaseClient {
       .update(data.toJson());
   Stream<QuerySnapshot>? getCrimeLocations() =>
       _firestore.collection(AppConstants.crimesLocationCollections).snapshots();
-
-  Future<List<List<String>>> uploadImages(
-      {@required List<Asset>? assets}) async {
-    List<String> uploadUrls = [];
-
-    return await Future.wait(
-        assets!.map((asset) async {
-          ByteData byteData = await asset.getByteData();
-          Uint8List imageData = byteData.buffer.asUint8List();
-          Reference reference = FirebaseStorage.instance.ref().child(
-              AppConstants.firebaseStorageBucket +
-                  "/${DateTime.now().toString()}");
-          UploadTask uploadTask = reference.putData(imageData);
-          TaskSnapshot storageTaskSnapshot;
-          await uploadTask.whenComplete(() {
-            uploadTask.then((TaskSnapshot snapshot) async {
-              storageTaskSnapshot = snapshot;
-              final String downloadUrl =
-                  await storageTaskSnapshot.ref.getDownloadURL();
-              uploadUrls.add(downloadUrl);
-              print("all images ====>  $uploadUrls");
-            }).catchError((Object e) {
-              log(e.toString());
-            });
-          });
-          return uploadUrls;
-        }),
-        eagerError: true, cleanUp: (_) {
-      log('eager cleaned up');
-    });
-  }
 
   Future<List<String>> uploadFiles(List<Asset> _images) async {
     var imageUrls =
