5bc473ed462023588d54414aac33a6fb3fdc4015
EricRootLee <erickiokom@gmail.com>
2021-07-26 01:07:03 +0300

    finished on splash screen

--- lib/src/provider/auth_provider.dart
+++ lib/src/provider/auth_provider.dart
@@ -16,27 +16,52 @@ class AuthProvider extends BaseProvider {
     ],
   );
 
-  User? userIsAuthenticated() => FirebaseAuth.instance.currentUser;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
   Future<void> handleSignIn() async {
     setBusy(true);
+    final GoogleSignInAccount? googleSignInAccount =
+        await googleSignIn.signIn();
+
+    final GoogleSignInAuthentication googleSignInAuthentication =
+        await googleSignInAccount!.authentication;
+    final AuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleSignInAuthentication.accessToken,
+      idToken: googleSignInAuthentication.idToken,
+    );
     try {
-      await googleSignIn.signIn().then((value) {
+      await _auth.signInWithCredential(credential).then(((user) async {
         setBusy(false);
-        print("Sign in value ============> $value");
-        if (value == null) {
-          setBusy(false);
-        } else {
-          UserModel signedUser =
-              UserModel(userEmail: value.email, userName: value.displayName);
-          setCurrentUser(signedUser);
-          FirebaseClient().saveUser(value.id, signedUser);
-        }
-      });
-    } catch (error) {
+        print("Sign in value ============> $user");
+
+        UserModel signedUser = UserModel(
+            userEmail: user.user!.email, userName: user.user!.displayName);
+        setCurrentUser(signedUser);
+        FirebaseClient().saveUser(user.user!.uid, signedUser);
+      }));
+    } catch (e) {
       setBusy(false);
-      print(error);
+      print(e);
     }
+
+    // setBusy(true);
+    // try {
+    //   await googleSignIn.signIn().then((value) {
+    //     setBusy(false);
+    //     print("Sign in value ============> $value");
+    //     if (value == null) {
+    //       setBusy(false);
+    //     } else {
+    //       UserModel signedUser =
+    //           UserModel(userEmail: value.email, userName: value.displayName);
+    //       setCurrentUser(signedUser);
+    //       FirebaseClient().saveUser(value.id, signedUser);
+    //     }
+    //   });
+    // } catch (error) {
+    //   setBusy(false);
+    //   print(error);
+    // }
   }
 
   onCurrentUserChanged() {
